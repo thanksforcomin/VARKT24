a4f4f631b4751a75ef045b64a4ad81f2c19eadb1
Megakret <tymatee@gmail.com>
2024-12-12 01:19:56 +0300

    Orbiting now works

--- kRPC/toOrbit.py
+++ kRPC/toOrbit.py
@@ -1,6 +1,6 @@
 import krpc
 from krpc.services.spacecenter import *
-from math import sqrt
+from math import sqrt, exp
 from time import sleep
 
 
@@ -21,8 +21,6 @@ def engage(connection: krpc.Client, ascentProfileConstant=1.25):
     vessel = space_center.active_vessel
 
     vessel.control.rcs = True
-    vessel.auto_pilot.sas = True
-    vessel.auto_pilot.sas_mode = SASMode.normal
     vessel.control.throttle = 0.76
 
     vessel.control.activate_next_stage()
@@ -30,8 +28,6 @@ def engage(connection: krpc.Client, ascentProfileConstant=1.25):
     apoapsisStream = connection.add_stream(getattr, vessel.orbit, "apoapsis_altitude")
     altitude = connection.add_stream(getattr, vessel.flight(), "mean_altitude")
     ut = connection.add_stream(getattr, connection.space_center, "ut")
-
-    vessel.auto_pilot.sas = True
     vessel.auto_pilot.engage()
     vessel.auto_pilot.target_heading = 90
     vessel.auto_pilot.target_pitch = 90
@@ -52,10 +48,12 @@ def engage(connection: krpc.Client, ascentProfileConstant=1.25):
         sleep(0.1)
 
     vessel.control.throttle = 0
+    sleep(1)
     timeToApoapsisStream = connection.add_stream(
         getattr, vessel.orbit, "time_to_apoapsis"
     )
     periapsisStream = connection.add_stream(getattr, vessel.orbit, "periapsis_altitude")
+    #calculating delta v
     mu = vessel.orbit.body.gravitational_parameter
     r = vessel.orbit.apoapsis
     a1 = vessel.orbit.semi_major_axis
@@ -63,42 +61,58 @@ def engage(connection: krpc.Client, ascentProfileConstant=1.25):
     v1 = sqrt(mu * ((2.0 / r) - (1.0 / a1)))
     v2 = sqrt(mu * ((2.0 / r) - (1.0 / a2)))
     delta_v = v2 - v1
+    #setting maneuver node and rotating to it
     node = vessel.control.add_node(ut() + timeToApoapsisStream(), prograde=delta_v)
-    vessel.auto_pilot.disengage()
-    vessel.auto_pilot.sas = True
-    vessel.control.sas_mode = SASMode.maneuver
-    print(vessel.control.sas_mode)
-    # Now, wait and perform circularization burn
-    while timeToApoapsisStream() > 22:
-        print(vessel.control.sas_mode)
-        sleep(0.5)
-
+    vessel.auto_pilot.reference_frame = node.reference_frame
+    vessel.auto_pilot.target_direction = (0,1,0)
+    vessel.control.throttle = 0.2
+    print(vessel.auto_pilot.reference_frame)
+    print(vessel.auto_pilot.target_direction)
+    print(node.reference_frame)
+    vessel.auto_pilot.wait()
+    # Calculate burn time (using rocket equation)
+    F = vessel.available_thrust
+    Isp = vessel.specific_impulse * 9.82
+    m0 = vessel.mass
+    m1 = m0 / exp(delta_v/Isp)
+    flow_rate = F / Isp
+    burn_time = (m0 - m1) / flow_rate   
+    vessel.control.throttle = 0
+    sleep(1)
     vessel.control.activate_next_stage()
+    print(burn_time)
+    # Now, wait and perform circularization burn
+    while node.time_to > burn_time/2:
+        print("Drifting to apoapsis.....")
+        sleep(0.5)
 
     vessel.control.throttle = 1
     lastUT = ut()
     lastTimeToAp = timeToApoapsisStream()
-    while node.remaining_delta_v > 0:
+    #Circulazation burn
+    while node.remaining_delta_v > 30:
         sleep(0.2)
+        print("Remaining delta V:", node.remaining_delta_v)
+        liquid_fuel = vessel.resources.amount("LiquidFuel")
         timeToAp = timeToApoapsisStream()
         deltaTimeToAp = (timeToAp - lastTimeToAp) / (ut() - lastUT)
-
+        vessel.auto_pilot.target_direction = (0,1,0)
         print("Estimated change in time to apoapsis per second:", deltaTimeToAp)
 
         # Adjust throttle according to the current deltaTimeToAp
-        if deltaTimeToAp < -0.3:
-            vessel.control.throttle += 0.03
-        elif deltaTimeToAp < -0.1:
-            vessel.control.throttle += 0.01
+        # if deltaTimeToAp < -0.3:
+        #     vessel.control.throttle += 0.03
+        # elif deltaTimeToAp < -0.1:
+        #     vessel.control.throttle += 0.01
 
-        if deltaTimeToAp > 0.2:
-            vessel.control.throttle -= 0.03
-        elif deltaTimeToAp > 0:
-            vessel.control.throttle -= 0.01
+        # if deltaTimeToAp > 0.2:
+        #     vessel.control.throttle -= 0.03
+        # elif deltaTimeToAp > 0:
+        #     vessel.control.throttle -= 0.01
 
         lastTimeToAp = timeToApoapsisStream()
         lastUT = ut()
-
+    node.remove()
     vessel.control.throttle = 0
     print("Apoapsis: ", apoapsisStream())
     print("Periapsis: ", periapsisStream())
